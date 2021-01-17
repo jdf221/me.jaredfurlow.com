758b076529333d0eba8a9309c3307ec31d4962ff
Jared Furlow <jdf2.furlow@gmail.com>
2021-01-16 18:25:58 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    "index.html",
+    "src/**/*.html",
+    "src/**/*.css",
+    "src/**/*.js"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

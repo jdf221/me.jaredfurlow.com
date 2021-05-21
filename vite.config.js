6fbc220fe6dff95eb440f9d7ba807fd913b50caf
Jared Furlow <jdf2.furlow@gmail.com>
2021-05-20 21:35:29 -0500

    Switch to index.hbs

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import { defineConfig } from "vite";
 import WindiCSS from "vite-plugin-windicss";
 import VitePluginFonts from "vite-plugin-fonts";
 import handlebars from "vite-plugin-handlebars";
+import customIndex from "vite-plugin-custom-index";
 
 export default defineConfig({
   plugins: [
+    customIndex("index.hbs"),
     WindiCSS({
       scan: { dirs: ["."], fileExtensions: ["html", "css", "hbs"] },
     }),

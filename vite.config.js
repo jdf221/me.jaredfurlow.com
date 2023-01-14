2be4f45ec298a4753660108aaf99c51a93d3e595
Jared Furlow <jdf2.furlow@gmail.com>
2023-01-14 01:11:58 -0600

    Move to GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import path from "path";
-
+import path from "node:path";
 import { defineConfig } from "vite";
-import customIndex from "vite-plugin-custom-index";
 import VitePluginFonts from "vite-plugin-fonts";
 import handlebars from "vite-plugin-handlebars";
 import WindiCSS from "vite-plugin-windicss";
 
 export default defineConfig({
   plugins: [
-    customIndex({
-      index: "index.hbs",
-      treatAsHtml: ["hbs"],
-    }),
     WindiCSS({
       scan: { dirs: ["."], fileExtensions: ["html", "css", "hbs"] },
     }),
